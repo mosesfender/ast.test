bb9a804ee09a3c0594ef842f77045d0c77af67df
Sergey Siunov <sergey@siunov.ru>
2024-06-19 15:24:16 +0300

    Первоначальное наведение порядка

--- config/db.php
+++ config/db.php
@@ -1,14 +1,20 @@
 <?php
 
-return [
+$result = [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=localhost;dbname=astdb',
+    'username' => 'ast',
+    'password' => 'alskdjfhg',
     'charset' => 'utf8',
 
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
 ];
+
+if (YII_ENV == YII_ENV_PROD) {
+    $result = array_merge($result, [
+        'enableSchemaCache' => true,
+        'schemaCacheDuration' => 3600,
+        'schemaCache' => 'cache',
+    ]);
+}
+
+return $result;
